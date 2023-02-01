845fd5be69d11739cbcb5535a12f2cec0a54fc31
Jay Joo <jj534@cornell.edu>
2023-02-01 07:31:34 -0500

    add components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-jay
+<button class="btn-primary btn">Create post</button>
+<div class="badge-secondary badge">secondary</div>
+<div class="chat chat-start">
+	<div class="chat-bubble">It's over Anakin, <br />I have the high ground.</div>
+</div>
+<div class="chat chat-end">
+	<div class="chat-bubble">You underestimate my power!</div>
+</div>
