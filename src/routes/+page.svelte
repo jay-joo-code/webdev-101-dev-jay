5e98ca3bd3a509886e8eaeba05bacaa501868f07
Jay Joo <jj534@cornell.edu>
2023-02-01 09:53:37 -0500

    conditionally render create post button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
-<div class="flex justify-center">
-	<!-- The button to open modal -->
-	<label for="my-modal-4" class="btn-primary btn">Create post</label>
+<script>
+	import { currentUser } from '$lib/pocketbase';
+</script>
 
-	<!-- Put this part before </body> tag -->
-	<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-	<label for="my-modal-4" class="modal cursor-pointer">
-		<label class="modal-box relative" for="">
-			<h3 class="text-lg font-bold">Create post</h3>
+{#if $currentUser}
+	<div class="flex justify-center">
+		<!-- The button to open modal -->
+		<label for="my-modal-4" class="btn-primary btn">Create post</label>
+
+		<!-- Put this part before </body> tag -->
+		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
+		<label for="my-modal-4" class="modal cursor-pointer">
+			<label class="modal-box relative" for="">
+				<h3 class="text-lg font-bold">Create post</h3>
+			</label>
 		</label>
-	</label>
-</div>
+	</div>
+{/if}
