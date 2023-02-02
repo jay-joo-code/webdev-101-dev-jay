26c70839728b63cf19931133263e3970fe22bdf1
Jay Joo <jj534@cornell.edu>
2023-02-02 13:05:01 -0500

    complete create post feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-	import { currentUser } from '$lib/pocketbase';
+	import { currentUser, pb } from '$lib/pocketbase';
+
+	let caption = '';
+	const formData = new FormData();
+
+	const handleFileChange = (event) => {
+		for (let file of event?.target?.files) {
+			formData.append('photo', file);
+		}
+	};
+
+	const handleCreatePost = async () => {
+		formData.append('caption', caption);
+		formData.append('user', $currentUser.id);
+		await pb.collection('posts').create(formData);
+		caption = '';
+	};
 </script>
 
 {#if $currentUser}
@@ -11,7 +27,23 @@
 		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
 		<label for="my-modal-4" class="modal cursor-pointer">
 			<label class="modal-box relative" for="">
-				<h3 class="text-lg font-bold">Create post</h3>
+				<div class="space-y-4">
+					<h3 class="text-lg font-bold">Create post</h3>
+					<div class="form-control">
+						<label class="label">
+							<span class="label-text">Caption</span>
+						</label>
+						<textarea class="textarea-bordered textarea h-24" bind:value={caption} />
+					</div>
+					<input
+						type="file"
+						class="file-input-bordered file-input w-full max-w-xs"
+						on:change={handleFileChange}
+					/>
+					<label for="my-modal-4" class="btn-primary btn-block btn" on:click={handleCreatePost}
+						>Create post</label
+					>
+				</div>
 			</label>
 		</label>
 	</div>
